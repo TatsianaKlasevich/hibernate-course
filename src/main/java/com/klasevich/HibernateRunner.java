7a0d9e7e16744f8dc0e153f3c4a4219e87eb0047
Tatsiana Klasevich <klasevich.t@gmail.com>
2022-03-01 00:04:06 +0300

    create entity

--- src/main/java/com/klasevich/HibernateRunner.java
+++ src/main/java/com/klasevich/HibernateRunner.java
@@ -1,10 +1,12 @@
 package com.klasevich;
 
+import com.klasevich.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) throws SQLException {
@@ -17,11 +19,24 @@ public class HibernateRunner {
 //        Session
 
         Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(User.class);
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstName("Ivan")
+                    .lastName("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
