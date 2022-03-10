e403efa3c61bd9907c977fe8f926b090db084bde
Tatsiana Klasevich <klasevich.t@gmail.com>
2022-03-10 19:45:18 +0300

    add mappedSuperclass

--- src/main/java/com/klasevich/entity/User.java
+++ src/main/java/com/klasevich/entity/User.java
@@ -38,7 +38,7 @@ import java.util.List;
 @Entity
 @Table(name = "users", schema = "public")
 @TypeDef(name = "klass", typeClass = JsonBinaryType.class)
-public class User implements Comparable<User> {
+public class User implements Comparable<User>, BaseEntity<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
